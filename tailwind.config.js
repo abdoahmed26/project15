c31b13d4a05434b4b985646de76251fdc5a1c468
Abdo <Abdo.gmail.com>
2023-09-27 17:32:00 +0300

    improve

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
       center: true,
       padding: "2rem",
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'mainBack': "url('/imgs/background-features.5f7a9ac9.jpg')",
+        'twoBack': "url('/imgs/background-faqs.55d2e36a.jpg')",
+      }
+    },
   },
   plugins: [],
 };
